24370d731597316aa4df86b6427202393bd49d21
EricGlez131 <150742332+EricGlez131@users.noreply.github.com>
2023-11-14 15:33:40 -0600

    all the test/ documentation work is done i think just needs a double check before turning it in

--- television.py
+++ television.py
@@ -4,7 +4,7 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         this method is the constructor for the objects and sets the pre values everytime the object is made
         """
@@ -14,7 +14,7 @@ class Television:
         self.prior = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
         """
         this method turn the tv on and off with a boolean value
         :return: a boolean value
@@ -24,7 +24,7 @@ class Television:
         else:
             self.__status = False
 
-    def mute(self):
+    def mute(self) -> None:
         """
         This method changes the boolean value for __muted and stores a value with the prior volume
         in order to turn the actual volume to zero, it only works if the __status Boolean is true
@@ -36,7 +36,7 @@ class Television:
                 self.prior = self.__volume
                 self.__volume = 0
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         """
         this method changes the channel by 1 up and if its max it becomes the MIN_CHANNEL just
         like a television, it only works if the __status Boolean is true
@@ -48,7 +48,7 @@ class Television:
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         """
         similar to channel_up this method turn the channel variable down by 1 and if the MIN_CHANNEL it becomes
         the MAX_CHANNEL it only works if the __status Boolean is true
@@ -60,7 +60,7 @@ class Television:
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         """
         this method turn the __volume variable up by one and if __muted status is True it changes it false and get the
         Previous volume before it was changed if it is the MAX_VOLUME  it stays the same,
@@ -76,7 +76,7 @@ class Television:
             else:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         """
         what this method does it similar to the volume_up it turn the volume down and also does the same with the __muted
         button and gets the previous volume if the volume is the MIN_VOLUME it stays the same
--- test_television.py
+++ test_television.py
@@ -5,32 +5,91 @@ from television import *
 class Test:
     def setup_method(self):
         self.tv1 = Television()
+        self.tv2 = Television()
 
     def teardown_method(self):
         del self.tv1
+        del self.tv2
 
     def test_init(self):
         assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
-        self.tv1.power()
-        self.tv1.channel_up()
-        self.tv1.volume_up()
-        self.tv1.volume_up()
-        assert self.tv1.__str__() == "Power = [True], Channel = [1], Volume = [2]"
+        assert self.tv2.__str__() == "Power = [False], Channel = [0], Volume = [0]"
 
     def test_power(self):
-        pass
+        self.tv1.power()
+        self.tv1.volume_up()
+        self.tv1.volume_up()
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [2], Volume = [2]"
+        self.tv1.power()
+        assert self.tv1.__str__() == "Power = [False], Channel = [2], Volume = [2]"
 
     def test_mute(self):
-        pass
+        self.tv1.power()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [1]"
+        self.tv1.mute()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [0]"
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [2]"
+        self.tv1.mute()
+        self.tv1.power()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
+        self.tv1.mute()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
 
     def test_channel_up(self):
-        pass
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
+        self.tv1.power()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [1], Volume = [0]"
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [3], Volume = [0]"
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [0]"
 
     def test_channel_down(self):
-        pass
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
+        self.tv1.power()
+        self.tv1.channel_down()
+        assert self.tv1.__str__() == "Power = [True], Channel = [3], Volume = [0]"
 
     def test_volume_up(self):
-        pass
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
+        self.tv1.power()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [1]"
+        self.tv1.mute()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [2]"
+        self.tv1.volume_up()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [2]"
 
     def test_volume_down(self):
-        pass
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == "Power = [False], Channel = [0], Volume = [0]"
+        self.tv1.power()
+        self.tv1.volume_up()
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [2]"
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [1]"
+        self.tv1.volume_up()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [2]"
+        self.tv1.mute()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [0]"
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [1]"
+        self.tv1.volume_down()
+        self.tv1.volume_down()
+        assert self.tv1.__str__() == "Power = [True], Channel = [0], Volume = [0]"
+
+
+
+
