9c03c205a3ca4a251039239f135c9b90f8289363
EricGlez131 <150742332+EricGlez131@users.noreply.github.com>
2023-11-13 18:22:53 -0600

    still needs some work

--- television.py
+++ television.py
@@ -11,22 +11,44 @@ class Television:
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        if not self.__status:
+            self.__status = True
+        else:
+            self.__status = False
 
     def mute(self):
-        pass
+        if not self.__muted:
+            self.__muted = True
+        else:
+            self.__muted = False
 
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel += 1
 
     def channel_down(self):
-        pass
+        if self.__status:
+            if self.__channel == Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel -= 1
 
     def volume_up(self):
-        pass
+        if self.__status:
+            if self.__volume == Television.MAX_VOLUME:
+                self.__volume = Television.MAX_VOLUME
+            else:
+                self.__volume += 1
 
     def volume_down(self):
-        pass
+        if self.__status:
+            if self.__volume == Television.MIN_VOLUME:
+                self.__volume = Television.MIN_VOLUME
+            else:
+                self.__volume -= 1
 
     def __str__(self):
         return f'Power = [{self.__status}], Channel = [{self.__channel}], Volume = [{self.__volume}]'
